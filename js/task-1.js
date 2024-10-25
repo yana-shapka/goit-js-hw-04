39530a4ab97e3c79876915b096e35af2813fc0f9
yana-shapka <57398010+yana-shapka@users.noreply.github.com>
2024-10-25 20:49:55 +0300

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,14 @@
+function slugify(title) {
+  let titleLowerCase = title.toLowerCase();
+  let titleUrl = titleLowerCase.replace(/ /g, '-');
+  if (titleUrl.length > 0) {
+    return titleUrl;
+  } else {
+    return 'The title is empty';
+  }
+}
+
+console.log(slugify('Arrays for begginers')); // "arrays-for-begginers"
+console.log(slugify('English for developer')); // "english-for-developer"
+console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
+console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
